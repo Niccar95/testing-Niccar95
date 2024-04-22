a83ef90ef58d570b086c4f640e4a46d4bcfb577d
Niccar95 <niccar.95@outlook.com>
2024-04-22 11:34:06 +0200

    first commit

---
+++ src/__tests__/functions.test.ts
@@ -0,0 +1,15 @@
+import { IMovie } from "../ts/models/Movie";
+
+describe("This function should sort movies", () => {
+  test("This should sort", () => {
+    const movies: IMovie[] = [
+      {
+        Title: "yo",
+        imdbID: "drfr",
+        Type: "ffr",
+        Poster: "dff",
+        Year: "fff",
+      },
+    ];
+  });
+});
