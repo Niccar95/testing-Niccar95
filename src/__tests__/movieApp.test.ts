6cabf321ca669b5aeac27f781e9b679b120817f8
Niccar95 <niccar.95@outlook.com>
2024-04-22 16:56:27 +0200

    tested displayNoResult function

--- src/__tests__/movieApp.test.ts
+++ src/__tests__/movieApp.test.ts
@@ -1,13 +1,19 @@
-import { getData } from "../ts/services/movieService";
+import { IMovie } from "../ts/models/Movie";
 import * as movieApp from "./../ts/movieApp";
+import * as movieService from "./../ts/services/movieService";
+import { handleSubmit } from "./../ts/movieApp";
 
 jest.mock("./../ts/services/movieService.ts");
 
 describe("testing init function", () => {
   let mockCreateHtml: jest.SpyInstance<void>;
+  let mockDisplayNoResult: jest.SpyInstance<void>;
+  let mockGetData: jest.SpyInstance<Promise<IMovie[]>>;
 
   beforeEach(() => {
     mockCreateHtml = jest.spyOn(movieApp, "createHtml");
+    mockDisplayNoResult = jest.spyOn(movieApp, "displayNoResult");
+    mockGetData = jest.spyOn(movieService, "getData");
 
     document.body.innerHTML = `<div id="app">
     <form id="searchForm">
@@ -19,13 +25,24 @@ describe("testing init function", () => {
   });
 
   test("it should call handleSubmit", async () => {
-    let searchText = "blah";
+    await handleSubmit();
 
-    movieApp.handleSubmit();
-    await getData(searchText);
     expect(mockCreateHtml).toHaveBeenCalled();
     const movieElements = document.getElementsByClassName("movie");
 
     expect(movieElements).toHaveLength(1);
+  });
+
+  test("it should displayNoResult", async () => {
+    mockGetData.mockImplementation((): Promise<IMovie[]> => {
+      return Promise.resolve([]);
+    });
+
+    await handleSubmit();
+    expect(mockDisplayNoResult).toHaveBeenCalled();
+
+    const movieElements = document.getElementsByClassName("movie");
+
+    expect(movieElements).toHaveLength(0);
   });
 });
