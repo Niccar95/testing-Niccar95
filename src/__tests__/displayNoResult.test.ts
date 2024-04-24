67ea1a3a5a837d3a9b424c44382e684afd37a7ee
Niccar95 <niccar.95@outlook.com>
2024-04-24 16:21:34 +0200

    added test for displayNoResult function

--- src/__tests__/displayNoResult.test.ts
+++ src/__tests__/displayNoResult.test.ts
@@ -1,1 +1,22 @@
-describe("Testing displayNoResult", () => {});
+import { displayNoResult } from "../ts/displayNoResult";
+
+describe("Testing displayNoResult", () => {
+  test("This should display a message", () => {
+    document.body.innerHTML = `<div id="app"><form id="searchForm">
+    <input type="text" id="searchText" placeholder="Skriv titel här" />
+    <button type="submit" id="search">Sök</button>
+  </form>
+  <div id="movie-container"></div>
+</div>`;
+
+    const noMessage = document.getElementsByTagName("p");
+
+    const container = document.getElementById(
+      "movie-container"
+    ) as HTMLDivElement;
+
+    displayNoResult(container);
+
+    expect(noMessage[0].innerHTML).toBe("Inga sökresultat att visa");
+  });
+});
